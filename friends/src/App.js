e40e8dcfc9ba17a704dd0d94d43eed7e12582ec7
willmondal <wbmondal@aol.com>
2021-01-26 15:00:48 -0800

    login now gets token with correct credentials

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
 import './App.css';
+
+import Login from './components/Login'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Link to='/login'>Login</Link><br/>
+        <Link>LogOut</Link>
+        <Switch>
+          <Route component={Login}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
